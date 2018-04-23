946651c59ab4e914b9eb1f956094c149eb8731d2
Chih-Wei Huang <cwhuang@linux.org.tw>
2018-04-23 10:01:12 +0800

    i965: more fixes for Google Play crashing

--- src/mesa/drivers/dri/i965/intel_mipmap_tree.c
+++ src/mesa/drivers/dri/i965/intel_mipmap_tree.c
@@ -3041,6 +3041,7 @@ intel_miptree_map(struct brw_context *brw,
 {
    struct intel_miptree_map *map;
 
+   if (!mt) return;
    assert(mt->num_samples <= 1);
 
    map = intel_miptree_attach_map(mt, level, slice, x, y, w, h, mode);
@@ -3087,6 +3088,7 @@ intel_miptree_unmap(struct brw_context *brw,
                     unsigned int level,
                     unsigned int slice)
 {
+   if (!mt) return;
    struct intel_miptree_map *map = mt->level[level].slice[slice].map;
 
    assert(mt->num_samples <= 1);
